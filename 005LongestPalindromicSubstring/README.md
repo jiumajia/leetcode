88d44809312c9ff5812ffad9472cdbdf3fae63a2
unknown <miaoluzeng@100me.com>
2017-11-02 14:13:47 +0800

    005 update

--- 005LongestPalindromicSubstring/README.md
+++ 005LongestPalindromicSubstring/README.md
@@ -1,4 +1,19 @@
+python version : Python3
 
+TEST case
 
+```
+1
+Input:"cbbd"
+Expected: "bb"
+
+2
+Input: "a"
+Expected: "a"
+
+3
+Input: "abcda"
+Expected: "a"
+```
 
 https://leetcode.com/problems/longest-palindromic-substring/description/
--- 005LongestPalindromicSubstring/main.py
+++ 005LongestPalindromicSubstring/main.py
@@ -4,19 +4,61 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        size = len(s)
+        if size == 1:
+            return s
+        if size == 2:
+            if s[0] == s[1]:
+                return s
+            return s[0]
+        maxp = 1
+        ans = s[0]
+        i = 0
+        while i < size:
+            j = i + 1
+            while j < size:
+                if s[i] == s[j]:
+                    j += 1
+                else:
+                    break
+            k = 0
+            while i - k - 1 >= 0 and j + k<= size - 1:
+                if s[i- k - 1] != s[j + k]:
+                    break
+                k += 1
+            if j - i + 2*k > maxp:
+                maxp = j- i + 2*k
+                ans = s[i - k:j + k]
+            if j + k == size - 1:
+                break
+            i = j
+        return ans
+
+    def longestPalindrome_test(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         temp_list = []
 
+        longest_count = 0
+        longest_str = ""
         for idx_item, item in enumerate(s):
-            if item in temp_list:
-                idx_first = temp_list.index(item)
+            for x in range(0, len(s[idx_item+1:])+1):
+                end_idx = idx_item+x+1
+                if s[idx_item:end_idx] == s[idx_item:end_idx][::-1]:
+                    if longest_count < len(s[idx_item:end_idx]):
+                        longest_str = s[idx_item:end_idx]
+                        longest_count = len(s[idx_item:end_idx])
+            if longest_count >= len(s[idx_item:]):
+                break
+
+        return longest_str
 
 
-        if max == 0:
-            return ""
-
-        return
 
 if __name__ == "__main__":
-    num = 'abc'
+    num = "kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv"
     s = Solution()
-    print s.longestPalindrome(num)
+    print(s.longestPalindrome(num))
+    print(s.longestPalindrome_test(num))
